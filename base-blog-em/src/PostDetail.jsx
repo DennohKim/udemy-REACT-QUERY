116f28babf81f915aef251737d767cef4a18c8e9
“dennohkim” <kimathidennis38@gmail.com>
2022-11-11 15:20:34 +0300

    feat: implement update feat mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -29,6 +29,8 @@ export function PostDetail({ post }) {
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
 
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
   
   if(isLoading) return <h3>Loading Comments ... </h3>
 
@@ -47,7 +49,10 @@ export function PostDetail({ post }) {
       {deleteMutation.isError && <p style={{color: 'red'}}>Error deleting the post</p>}
       {deleteMutation.isLoading && <p style={{color: 'purple'}}>Deleting the post</p>}
       {deleteMutation.isSuccess && <p style={{color: 'green'}}>The post has been deleted</p>}
-      <button>Update title</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+      {updateMutation.isError && <p style={{color: 'red'}}>Error updating the post</p>}
+      {updateMutation.isLoading && <p style={{color: 'purple'}}>Updating the post</p>}
+      {updateMutation.isSuccess && <p style={{color: 'green'}}>The post has been updated</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
