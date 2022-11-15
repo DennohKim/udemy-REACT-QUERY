fd3d2908604f7b82ac2618ede9f61e0d22eb61b3
“dennohkim” <kimathidennis38@gmail.com>
2022-11-15 08:41:28 +0300

    call use infinite scroller hook

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
+
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -9,5 +11,13 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+
+  //fetchNextPage is a function that runs when we want more data to be fetched
+  //hasNextPage is a boolean that check if there is any more pages for data to collect
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    { getNextPageParam: (lastPage) => lastPage.next || undefined  }
+  );
   return <InfiniteScroll />;
 }
